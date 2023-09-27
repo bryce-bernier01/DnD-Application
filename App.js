49b6a86a821ac52342e32e2d62fc952c68019f6c
Bryce Bernier <bryce.bernier@stonewain.com>
2023-09-26 22:09:47 -0400

    Successful commit!

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>We make a DnD App</Text>
+      <Text>Successfully Added to GitHub!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
